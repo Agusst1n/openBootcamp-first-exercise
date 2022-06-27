be61f4c42166f055bbd2a72fed5b8702fe62fcab
Nitsuga <88418234+Agusst1n@users.noreply.github.com>
2022-06-27 14:36:39 -0300

    first commit

---
+++ Javascript/Ejercicio7/conjuntos.js
@@ -0,0 +1,17 @@
+//*! Crea un archivo llamado conjuntos.js que contenga las siguientes líneas
+
+//*! - Un nuevo Set con los nombres de tu familia
+
+//*! - Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+
+//*! - Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+
+
+
+let set = new Set(['Romina','Gaston','Kiara','Zoe','Tom', 'Agustin']);
+
+set.add('Agustin')
+
+set.add('Javascript')
+
+
